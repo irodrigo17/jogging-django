6846420fbe167737e9f38cc39b2690f1429a9318
Ignacio Rodrigo <irodrigo17@gmail.com>
2015-07-25 10:02:45 -0300

    Add basic JSON web token authentication

--- jogging/settings.py
+++ jogging/settings.py
@@ -112,8 +112,15 @@ STATICFILES_DIRS = (
 # http://www.django-rest-framework.org
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
-    'PAGE_SIZE': 10
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAdminUser',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
+    'PAGE_SIZE': 10,
 }
 
 
--- jogging/urls.py
+++ jogging/urls.py
@@ -25,5 +25,6 @@ router.register(r'jogs', views.JogViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', 'rest_framework_jwt.views.obtain_jwt_token'),
 ]
